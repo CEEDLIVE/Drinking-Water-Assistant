725be286073243a9927be74152f6ef3b4b7e31a0
hanmo <dlgksah@gmail.com>
2018-05-16 18:50:59 +0900

    create project

---
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/MainActivity.kt
@@ -0,0 +1,12 @@
+package hanmo.com.drinkingwaterassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

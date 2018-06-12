8ee019d75955cd98c3fded3b8a49da8ec4a3ca93
hanmo <dlgksah@gmail.com>
2018-06-12 12:55:52 +0900

    디자인을 받아서 다시 구현 중 스플래시 액티비티 수정 완료 새로운 섭취 값 받는 액티비티 구현 중

---
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/MyTargetWaterActivity.kt
@@ -0,0 +1,15 @@
+package hanmo.com.drinkingwaterassistant
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by hanmo on 2018. 6. 12..
+ */
+class MyTargetWaterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mytarget)
+    }
+}
--- app/src/main/java/hanmo/com/drinkingwaterassistant/SplashActivity.kt
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/SplashActivity.kt
@@ -2,37 +2,33 @@ package hanmo.com.drinkingwaterassistant
 
 import android.animation.Animator
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.splashscreen.*
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+
+
 
 /**
  * Created by hanmo on 2018. 5. 28..
  */
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_DISPLAY_LENGTH : Long = 1500
+
     /** 처음 액티비티가 생성될때 불려진다.  */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
 
-        with(splashLottie) {
-            playAnimation()
-            speed = 1f
-            addAnimatorListener(object : Animator.AnimatorListener {
-                override fun onAnimationRepeat(animation: Animator?) {}
+        Handler().postDelayed({
+            /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+            val mainIntent = android.content.Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH)
 
-                override fun onAnimationEnd(animation: Animator?) {
-                    val mainIntent = android.content.Intent(this@SplashActivity, MainActivity::class.java)
-                    startActivity(mainIntent)
-                    finish()
-                }
-
-                override fun onAnimationCancel(animation: Animator?) {}
-
-                override fun onAnimationStart(animation: Animator?) {}
-
-            })
-        }
     }
 
 }

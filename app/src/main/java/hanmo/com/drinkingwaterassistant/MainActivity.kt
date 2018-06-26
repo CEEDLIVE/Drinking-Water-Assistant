3810f96abcc5af081965f47c719e314260eb85fe
hanmo <dlgksah@gmail.com>
2018-06-26 23:50:36 +0900

    DB와 layout 연동

--- app/src/main/java/hanmo/com/drinkingwaterassistant/MainActivity.kt
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/MainActivity.kt
@@ -1,5 +1,6 @@
 package hanmo.com.drinkingwaterassistant
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import hanmo.com.drinkingwaterassistant.lockscreen.util.Lockscreen
@@ -11,16 +12,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var compositeDisposable: CompositeDisposable
+    private lateinit var compositeDisposable: CompositeDisposable
+    private var waterTable : Goals? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         compositeDisposable = CompositeDisposable()
+    }
 
+    override fun onResume() {
+        super.onResume()
 
+        todayWaterText.setOnClickListener {
+            startActivity(MyTargetWaterActivity.newIntent(this@MainActivity))
+        }
+
+        waterTable = RealmHelper.instance.queryFirst(Goals::class.java)
         setSwitch()
+        setProgressBar()
+    }
+
+    private fun setProgressBar() {
+        waterTable?.let {
+            waterGoal.text = it.goal?.toString()
+            todayWater.text = it.today?.toString()
+            val percent : Int = (100 * (it.today!!.toDouble() / it.goal!!.toDouble())).toInt()
+            waterPercent.text = "$percent%"
+            waterProgressbar.max = it.goal!!
+            waterProgressbar.progress = it.today!!
+            todayLeftWaterText.text = "목표량까지${it.goal!! - it.today!!}ml 남았어요!"
+        }
     }
 
     private fun setSwitch() {

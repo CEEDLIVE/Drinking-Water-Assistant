d3f6eeedc7b54e5ec63e32ad2a83bd75f5848120
hanmo <dlgksah@gmail.com>
2018-07-23 23:49:07 +0900

    전체 히스토리 화면 생성

--- app/src/main/java/hanmo/com/drinkingwaterassistant/fragment/HistoryFragment.kt
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/fragment/HistoryFragment.kt
@@ -1,5 +1,21 @@
 package hanmo.com.drinkingwaterassistant.fragment
 
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hanmo.com.drinkingwaterassistant.R
+
 /**
  * Created by hanmo on 2018. 7. 19..
  */
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater?.inflate(R.layout.fragment_history, container, false)
+
+        return rootView
+    }
+
+}

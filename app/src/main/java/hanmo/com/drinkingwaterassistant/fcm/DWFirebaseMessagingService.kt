746ab63fa31cc709f6fa3cf9fb9ce3587915307f
hanmo <dlgksah@gmail.com>
2018-08-22 18:20:13 +0900

    FCM 및 노티 채널 오레오 대응 완료

--- app/src/main/java/hanmo/com/drinkingwaterassistant/fcm/DWFirebaseMessagingService.kt
+++ app/src/main/java/hanmo/com/drinkingwaterassistant/fcm/DWFirebaseMessagingService.kt
@@ -1,17 +1,15 @@
 package hanmo.com.drinkingwaterassistant.fcm
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.media.RingtoneManager
 import android.os.Build
 import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import hanmo.com.drinkingwaterassistant.DWApplication
 import hanmo.com.drinkingwaterassistant.R
 import hanmo.com.drinkingwaterassistant.main.MainActivity
 import hanmo.com.drinkingwaterassistant.notification.MyNotificationManager
@@ -44,35 +42,44 @@ class DWFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
+
     private fun sendNotification(body: String?) {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             putExtra("Notification", body)
         }
 
-        val CHANNEL_ID = "DRINK WATER CHANNEL ID"
-        val CHANNEL_NAME = "DRINK WATER CHANNEL NAME"
-        val description = "This is Drink Water channel"
-        val importance = NotificationManager.IMPORTANCE_HIGH
-
-        val notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             MyNotificationManager(this@DWFirebaseMessagingService).createMainNotificationChannel()
         }
 
+        startForeground(DWApplication.notificationId, createNotificationCompatBuilder(body, intent).build())
+    }
+
+    private fun createNotificationCompatBuilder(body: String?, intent: Intent): NotificationCompat.Builder {
+
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentTitle("Colloc Notification")
-                .setContentText(body)
-                .setAutoCancel(true)
-                .setSound(notificationSound)
-                .setContentIntent(pendingIntent)
-
-        notificationManager.notify(0, notificationBuilder.build())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //커스텀 화면 만들기
+            return NotificationCompat.Builder(this@DWFirebaseMessagingService, MyNotificationManager(this@DWFirebaseMessagingService).getMainNotificationId())
+                    .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("Drinking Water Notification")
+                    .setContentText(body)
+                    .setAutoCancel(true)
+                    .setSound(notificationSound)
+                    .setContentIntent(pendingIntent)
+        } else {
+            return NotificationCompat.Builder(this@DWFirebaseMessagingService,"Notification")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("Push Notification FCM")
+                    .setContentText(body)
+                    .setAutoCancel(true)
+                    .setSound(notificationSound)
+                    .setContentIntent(pendingIntent)
+        }
     }
+
 }
